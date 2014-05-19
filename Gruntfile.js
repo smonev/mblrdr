03906c2daba5516ce5aec0fb575ddde10c4f6150
Stoicho Monev <smonev@gmail.com>
2014-05-19 16:46:11 +0300

    test commit

---
+++ Gruntfile.js
@@ -0,0 +1,90 @@
+// ctrl shift p => grunt => deploy
+// wrapper function
+module.exports = function(grunt){
+	// load all our Grunt plugins
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-concat');  
+  grunt.loadNpmTasks('grunt-gae');
+  //grunt.loadNpmTasks('grunt-appengine');  
+
+  grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        // task configuration goes here
+ 
+        clean: {
+          all: ["build"],
+          temp: ["build/temp"],
+          js: ["build/js"]
+        },        
+        
+        copy: {
+          main: {
+            files: [ 
+              {
+                expand: true, flatten: true, src: ['src/js/*'], dest: 'build/js/', filter: 'isFile'
+              },
+              {
+                expand: true, flatten: true, src: ['src/js/ext/*'], dest: 'build/js/ext/', filter: 'isFile'
+              },
+              {
+                expand: true, flatten: true, src: ['src/js/ext/cultures/*'], dest: 'build/js/ext/cultures', filter: 'isFile'
+              },
+              {
+                expand: true, flatten: true, src: ['src/css/**'], dest: 'build/css/', filter: 'isFile'
+              },
+              {
+                expand: true, flatten: true, src: ['src/fonts/**'], dest: 'build/fonts/', filter: 'isFile'
+              },
+              {
+                expand: true, flatten: true, src: ['src/html/**'], dest: 'build/html/', filter: 'isFile'
+              },
+              {
+                expand: true, flatten: true, src: ['src/*.py', 'src/*.yaml'], dest: 'build/', filter: 'isFile'
+              },
+             ]
+          },        
+        },
+        
+        uglify: {
+            options: {
+                mangle: false
+            },
+            my_target: {
+                files: [{
+                    expand: true,
+                    flatten: true, 
+                    cwd: 'temp/js',
+                    src: '**/*.js',
+                    dest: 'temp/js'
+                }]
+            },
+        },
+        
+        concat: {
+          options: {
+            separator: ';',
+          },
+          dist: {
+            src: ['temp/**/*.js'],
+            dest: 'build/js/<%= pkg.name %>.js'          
+          },
+        },  
+        
+        gae: {
+            deploy_default: {
+                action: 'update',
+                options: {
+                    path: 'build/'
+                }            
+            }
+        }
+    });
+ 
+    // define the default task that executes when we run 'grunt' from inside the project
+    grunt.registerTask('dev', ['clean:all', 'copy', 'gae']);
+    grunt.registerTask('release', ['clean:all', 'copy', 'uglify', 'clean:js', 'concat', 'clean:temp', 'gae']);
+    
+    grunt.registerTask('default', ['dev']);
+};
---
+++ readme.txt
@@ -0,0 +1,15 @@
+How to setup your own mblrdr rss reader?
+
+1. Go to appengine.com and register (if you don't have registration).
+
+2. After registering, add a python app. Choose defautls and remember the app name.  
+
+3. Download and unzip the mblrdr.zip
+
+4. Open app.yaml and set replace 'ENTER YOUR APPLICATION NAME HERE' with your name from 2)
+
+5. Upload appengine by opening cmd prompt and write: "appcfg.py update mblrdr"  
+  (asuming the app.yaml file is in mblrdr folder)
+  (asuming you have python in your path)
+
+6. go to yourappname.appspot.com and you are ready.
