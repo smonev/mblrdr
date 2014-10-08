4f97d9a0c838499528d9b9b1db9b24d7b0620974
Stoicho Monev <smonev@gmail.com>
2014-10-08 23:51:20 +0300

    swipe fixes

--- build/js/shortcuts.js
+++ build/js/shortcuts.js
@@ -294,7 +294,7 @@ MblRdr.shortcuts = function() {
                 if ((e.deltaX > 100) || (e.deltaX < -100)) {
                     showLoader = true;
                     if ($progress.length === 0) {
-                        $progress = $('<div id="progress" style="position: fixed; z-index: 1; top: 0px; height: 2px;background: red; width:' + screenWidth + 'px;"/>').insertBefore('.articlesHeader');;
+                        $progress = $('<div id="progress" style="position: fixed; z-index: 1; top: 0px; height: 2px;background: red; width:' + screenWidth + 'px;transform: "translate3d("-' + screenWidth + '"px, 0, 0)"/>').insertBefore('.articlesHeader');;
                     }
                 }
             }
@@ -379,3 +379,38 @@ MblRdr.shortcuts = function() {
         gotoTop: gotoTop
     }
 }();
+
+
+var overscroll = function(el) {
+  el.addEventListener('touchstart', function() {
+    var top = el.scrollTop, totalScroll = el.scrollHeight, currentScroll = top + el.offsetHeight;
+
+    // If we're at the top or the bottom of the containers
+    // scroll, push up or down one pixel. 
+    // This prevents the scroll from "passing through" tothe body.
+    
+    if(top === 0) {
+      el.scrollTop = 1
+    } else if(currentScroll === totalScroll) {
+      el.scrollTop = top - 1
+    }
+  })
+
+  el.addEventListener('touchmove', function(evt) {
+    //if the content is actually scrollable, i.e. the content is long enough
+    //that scrolling can occur
+    if(el.offsetHeight < el.scrollHeight)
+      evt._isScroller = true
+  })
+}
+
+overscroll(document.querySelector('body'));
+
+document.body.addEventListener('touchmove', function(evt) {
+  //In this case, the default behavior is scrolling the body, which
+  //would result in an overflow.  Since we don't want that, we preventDefault.
+  if(!evt._isScroller) {
+    evt.preventDefault();
+    alert('no overflow for you');
+  }
+})
